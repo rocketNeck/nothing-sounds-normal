8ac91724e5e2a36cb82a2343732bf77d0e6db32d
Jesse L Lawson <homestarflyer@gmail.com>
2016-08-31 17:54:41 -0400

    added and config babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('My soundcloud react redux app')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Stream from './components/Stream';
+
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some other track'
+  }
+];
+
+ReactDOM.render(
+  <Stream tracks={tracks} />,
+  document.getElementById('app')
+);
