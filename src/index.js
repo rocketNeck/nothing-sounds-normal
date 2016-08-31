246e7c9c60ad29fc81539eaf4d78655f1385ac5a
Jesse L Lawson <homestarflyer@gmail.com>
2016-08-31 18:31:08 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
 import Stream from './components/Stream';
 
 const tracks = [
@@ -10,8 +12,10 @@ const tracks = [
     title: 'Some other track'
   }
 ];
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
-  <Stream tracks={tracks} />,
+  <Stream />,
   document.getElementById('app')
 );
